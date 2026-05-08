edac157bf9382dd1ae38825d5f29c1e4f1ab9b00
Emmy <famuyiwaemmanuel565@gmail.com>
2026-05-08 17:43:38 +0100

    fixed layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <>
       <main className="flex flex-col justify-center items-center">
-        <p>Time to test the app</p>
-        <Link href="/test">Testing</Link>
+        <p>Emmanuel</p>
       </main>
     </>
   );
