ff9730dd8fb39d015ee3986740ceefc793eb097f
Emmy <famuyiwaemmanuel565@gmail.com>
2026-05-10 23:30:24 +0100

    development in progress

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 
 const NavBar = () => {
-  return <div></div>;
+  return <div>Still in the process of development</div>;
 };
 
 export default NavBar;
