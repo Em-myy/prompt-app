4ae7df5484094ac82e16c9d67120143b691c107b
Emmy <famuyiwaemmanuel565@gmail.com>
2026-05-17 22:55:42 +0100

    tenth

---
+++ app/(main)/profile/page.tsx
@@ -0,0 +1,31 @@
+import LogoutButton from "@/components/LogoutButton";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+const ProfilePage = async () => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (!user || error) {
+    redirect("/login");
+  }
+  return (
+    <main>
+      <h1>Profile Page</h1>
+      <div>
+        <LogoutButton />
+      </div>
+      <section>
+        <p>
+          You have successfullty logged in <span>{user.email}</span>
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default ProfilePage;
