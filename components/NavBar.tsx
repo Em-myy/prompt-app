db46c8549fc51786454e9f1557522b30a742d792
Emmy <famuyiwaemmanuel565@gmail.com>
2026-05-17 22:30:24 +0100

    now navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,10 +2,61 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useAuth } from "@/context/AuthContext";
+import logoImage from "../public/assets/images/logo.svg";
+import { useRouter } from "next/navigation";
 
 const NavBar = () => {
-  return <h1>Navbar</h1>;
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/login");
+  };
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src={logoImage}
+          alt="Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Prompt App</p>
+      </Link>
+
+      <div className="sm:flex hidden">
+        {user && !loading ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signout} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src={logoImage}
+                alt="Profile Image"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            <button onClick={handleLogin} type="button" className="black_btn">
+              Login
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
