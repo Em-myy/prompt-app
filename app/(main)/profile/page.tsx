dfb77441149617ee27978eae1db6304ad03e54a5
Emmy <famuyiwaemmanuel565@gmail.com>
2026-05-21 22:13:50 +0100

    fixed profile

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -13,13 +13,19 @@ const ProfilePage = async () => {
   if (!user || error) {
     redirect("/login");
   }
+
+  const displayName =
+    user.user_metadata.display_name ||
+    user.user_metadata.full_name ||
+    user.user_metadata.username ||
+    user.email;
   return (
     <main>
       <h1>Profile Page</h1>
       <div></div>
       <section>
         <p>
-          You have successfullty logged in <span>{user.email}</span>
+          You have successfullty logged in <strong>{displayName}</strong>
         </p>
       </section>
     </main>
