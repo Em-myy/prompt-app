3cf185f3c5b0a98948504b70479f7da8106c9ca9
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2026-07-06 23:53:06 +0100

    Update text in CreatePrompt component

--- app/(main)/create-prompt/page.tsx
+++ app/(main)/create-prompt/page.tsx
@@ -1,5 +1,5 @@
 const CreatePrompt = () => {
-  return <div>Create more hmmm ano omoh another new</div>
+  return <div>Create almost more hmmm ano omoh another new</div>
 };
 
 export default CreatePrompt;
